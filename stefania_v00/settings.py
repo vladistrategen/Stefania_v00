1b2d2961d350259bd31cdadc24e71c7798613624
vladistrategen <100209446+vladistrategen@users.noreply.github.com>
2022-07-16 01:56:58 +0300

    Created filter app views for filtering the appointments by the desired doctor. Succesfully linked the filter views to html templates

--- stefania_v00/settings.py
+++ stefania_v00/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -34,6 +35,7 @@ INSTALLED_APPS = [
     'patient',
     'doctors',
     'appointment',
+    'filter',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'stefania_v00.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
