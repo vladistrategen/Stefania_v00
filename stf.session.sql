2da440bf516d24dade54fc232b7520a976e388d7
vladistrategen <vlad.istrate02@gmail.com>
2022-07-25 18:44:54 +1000

    rerouted urls, arch compatibility

--- stf.session.sql
+++
@@ -1,8 +0,0 @@
-
--- create a table with something
--- @BLOCK
-CREATE TABLE IF NOT EXISTS `test` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
