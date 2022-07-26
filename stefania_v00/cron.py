dbc5d457f9548653a8366305d23ef61a3fc295ab
vladistrategen <vlad.istrate02@gmail.com>
2022-07-26 15:22:18 +1000

    added crontab job scheduler

---
+++ stefania_v00/cron.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+
+def execute_every_minute():
+    # write the current time to the time.txt file in the dir
+    f=open("/home/vladi/Desktop/time.txt", "w")
+    f.write(str(datetime.now()))
+    f.close()
+    print('cron job executed')
+    return 'cron job executed'
