f8a65446431a1b87886f909a6dbd825fda94a212
vladistrategen <100209446+vladistrategen@users.noreply.github.com>
2022-08-28 22:19:40 +0300

    creted tools module

---
+++ frontend/src/tools/FetchTools.js
@@ -0,0 +1,73 @@
+import React, {useState,useEffect} from "react";
+
+
+// define a function to parse the data from the server and return an array of appointments as json objects
+const  parseDataMultiple = (data) => {
+    let parsedData = [];
+    
+   
+    for (let i = 0; i < data.length; i++) {
+        const parsedStartDate=new Date(data[i].date+"T"+data[i].time+".000Z");
+        const parsedEndDate=new Date();
+        parsedEndDate.setTime(parsedStartDate.getTime()+parseInt(data[i].duration_in_minutes)*60000);
+        parsedData.push({
+            "id": data[i].id,
+            "description": data[i].description,
+            "startDate": parsedStartDate.toISOString(),
+            "endDate": parsedEndDate.toISOString(),
+            "doctorId": data[i].doctor,
+            "patientId": data[i].patient,
+            "confirmation_status": data[i].confirmation_status,
+            "completed": data[i].completed,
+        });
+    }
+    return parsedData;
+}
+
+const parseDataSingle = (data) => {
+    const parsedStartDate = new Date(data.date + "T" + data.time + ".000Z");
+    const parsedEndDate = new Date();
+    parsedEndDate.setTime(parsedStartDate.getTime() + parseInt(data.duration_in_minutes) * 60000);
+    return({
+      "id": data.id,
+      "description": data.description,
+      "startDate": parsedStartDate.toISOString(),
+      "endDate": parsedEndDate.toISOString(),
+      "doctorId": data.doctor,
+      "patientId": data.patient,
+      "confirmation_status": data.confirmation_status,
+      "completed": data.completed,
+    });
+  }
+            
+/*
+const Fetchtest = () => {
+    const [appointments, setAppointments] = useState([]);
+    useEffect(() => {
+        getAppointments()
+    }, []);
+    
+    let getAppointments = async () => {
+        const res = await fetch('http://127.0.0.1:8000/api/appointments');
+        const data = await res.json();
+        
+        const parsedData = parseDataMultiple(data);
+        //console.log(parsedData);
+        setAppointments(parsedData);
+
+    }
+    
+    return (
+        <div>
+            {appointments.map(appointment => (
+                <div key={appointment.id}>
+                    {appointment.title}
+                </div>
+            ))}
+        </div>
+    );
+
+
+}*/
+
+export {parseDataMultiple,parseDataSingle};
