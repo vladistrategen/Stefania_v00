15699cff24c13124814bb164c8f6b202f0fd09b3
vladistrategen <100209446+vladistrategen@users.noreply.github.com>
2022-10-15 23:24:13 +0300

    removed middle_name field

--- patient/models.py
+++ patient/models.py
@@ -10,7 +10,6 @@ from django.db import models
 class Patient(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    middle_name = models.CharField(max_length=30, blank=True, null=True)
     birth_date = models.DateField(default=datetime.now)
     
     phone_number = models.CharField(max_length=15)
