ae787e6aab091b9b72f05e779ec7532c5c0f6a9b
vladistrategen <100209446+vladistrategen@users.noreply.github.com>
2022-10-20 11:06:24 +0300

    added color field for drs

--- doctors/models.py
+++ doctors/models.py
@@ -8,6 +8,8 @@ class Doctor(models.Model):
     last_name = models.CharField(max_length=30)
     email = models.EmailField(max_length=254)
     phone_number = models.CharField(max_length=15)
+    # preferred color for the doctor takes a hex value
+    preferred_color = models.CharField(max_length=7, default="#FFFFFF")
 
     def __str__(self):
         return self.first_name + " " + self.last_name
